f315f5717f33390e1f5ae0a68712c9753e11ddda
gajeong <rlarkwjd127@gmail.com>
2023-08-03 18:07:10 +0900

    stlye 모듈을 import 하기 위해서 typescript에서 설정해줘야 하는 파일

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,21 @@
 import { useState } from 'react'
-
+import { TiThMenuOutline } from 'react-icons/ti'
+import { TbCalendarHeart } from 'react-icons/tb'
+import styles from './header.module.css'
 export default function Header() {
   const [user, setUser] = useState('7rgoong')
-  return <header>{user}</header>
+
+  return (
+    <header className='font text-[#8a8a8a] flex'>
+      <div className=' p-4 '>{user}</div>
+      <div className=''>
+        <TiThMenuOutline className={styles.item} />
+        <p>Menu</p>
+      </div>
+      <div className=''>
+        <TbCalendarHeart className='' />
+        <p>Projects</p>
+      </div>
+    </header>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{tsx,ts,js,jsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {
+      base: '#1a1a1a',
+      'base-bright': '#a1a1a1',
+    },
   },
   plugins: [],
 }
